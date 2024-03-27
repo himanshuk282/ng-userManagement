d6524aefbb78ff6768cc9c73667c7f56e2c7a3c9
Himanshu Singh <65010539+himanshuk282@users.noreply.github.com>
2024-03-27 20:38:42 +0530

    Update app.config.ts to hide secrets

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,12 +9,12 @@ import {provideFirestore,getFirestore} from '@angular/fire/firestore'
 import { routes } from './app.routes';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvdeXhmp4GZHwCXajwLzdygWSM8KI9N0U",
+  apiKey: "APIKEY",
   authDomain: "ng-user-management-2b1df.firebaseapp.com",
   projectId: "ng-user-management-2b1df",
   storageBucket: "ng-user-management-2b1df.appspot.com",
   messagingSenderId: "905204268571",
-  appId: "1:905204268571:web:3890bbe986502d4655c2be"
+  appId: "AppID"
 }
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),importProvidersFrom([
